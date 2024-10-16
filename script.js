68d753b213490a8fd6360bbe5c685c991abbbeb3
Fabian4133 <e263226@edu.sbl.ch>
2024-10-16 14:38:56 +0000

    Aufgabe 1

--- script.js
+++ script.js
@@ -28,9 +28,13 @@ export function aufgabe01(args) {
     // `currentElement`, damit es explizit ist womit wir arbeiten, aber auch
     // damit wir nicht so viele Klammern schreiben müssen.
     const currentElement = input[i]
+    if (currentElement === "e") {
+      //do nothing
+    } else {
+      result.push(currentElement)
+    }
 
     // Hier wird das aktuelle Zeichen ans Ende der Resultat-Liste angehängt.
-    result.push(currentElement)
   }
 
   // Hier geben wir das Resultat zurück, und machen einen Text daraus.
